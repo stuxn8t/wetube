0e384adc8b3e8888c5994a9cd283532cac50182e
hocrux <hocrux@hocruxui-MacBook-Pro.local>
2020-12-04 02:15:47 +0900

    end webpack

--- app.js
+++ app.js
@@ -11,18 +11,20 @@ import routes from "./routes";
 
 const app = express();
 
-
-
-app.use(helmet());
-app.set('view engine',"pug");
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieparser());
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true}));
-app.use(morgan("dev"))
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
-app.use(localsMiddleware)
-
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter); // => spec: globalRouter.js
 app.use(routes.users, userRouter);
